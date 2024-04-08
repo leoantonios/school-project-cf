5e94da215fa32fbe18728ea40a038c42e809b3b4
Leonardo <leobuchini@gmail.com>
2024-04-07 21:47:00 -0500

    Añadiendo React Hook Form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 
 export default function Register() {
   return (
@@ -7,10 +6,10 @@ export default function Register() {
         <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
           <div>
             <h1 className="text-3xl font-bold text-center mb-4 cursor-pointer text-black">
-              Inicia Sesión
+              Registrese
             </h1>
             <p className="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide cursor-pointer">
-              ¡Ingrese sus creedenciales!
+              ¡Ingrese la siguiente informacion para registrarse!
             </p>
           </div>
           <div className="space-y-4">
@@ -27,12 +26,9 @@ export default function Register() {
           </div>
           <div className="text-center mt-6">
             <button className="w-full py-2 text-xl text-white bg-purple-400 rounded-lg hover:bg-purple-500 transition-all">
-              Ingresar
+              Registrarse
             </button>
-            <p className="mt-4 text-sm">
-                Quiere una Cuenta?{" "}
-              <Link className="underline  cursor-pointer" to="/"> Registrate</Link>
-            </p>
+
           </div>
         </div>
 
